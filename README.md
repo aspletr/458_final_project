ca135782ddd2859cf78635d828a040adadc13ae6
Lawrence <Lawrence03@users.noreply.github.com>
2024-03-08 01:49:38 -0800

    Changes

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-# 458_final_project
-Final Project
+# GEOG 458 Final Project: COVID-19 Mapping in the U.S.
 
-Web Link: https://aspletr.github.io/458_final_project/
+Reginald R Asplet, Carter Dainel Burr, Caden Ty Henson, Yanyi Li
+
+Winter 2024, Group AB4
+
+[Link to the live map](https://aspletr.github.io/458_final_project/)
+
+## Project Description
+
+Our final project is to make a Map-based storytelling application of COVID cases across the United States. This map is intended to help the user understand the impact that COVID-19 has had across different regions of the United States, helping visualize how the pandemic swept across the country. This map also intends to examine the policies  that were in place as well as the precautions that were taken in each of these states, showing how they might have impacted the Covid trends in that state.
+
+## Screenshots
+
+![Screenshot 1](img/screenshot1.png)
+![Screenshot 2](img/screenshot2.png)
+
+## Project Goals
+
+This digital platform will serve as a tool for public health officials, healthcare providers, and the general public to track the most recent COVID-19 trends, track the spread of the disease, and make informed decisions that can better help the public and hospitals. Examination of how the policies play a role in Covid trends based across the country will also help officials form future plans if an outbreak occurs in the future. Officials will be able to see what policies are the most effective to reduce the spread, as well as the policies that proved to be less effective.  The most important function of our project is leveraging geographic data and storytelling techniques to highlight spatial relationships and create a deeper understanding of the impact of COVID-19. To do this we will need to implement various features into our storytelling map, such as pictures and videos and a series of maps to tell our story. The maps can dive into different states and regions across the US and show how certain areas were more impacted than others.
+
+## Data Sources
+
+- [New York Times COVID-19 Data](https://github.com/nytimes/covid-19-data)
+
+## Applied Libraries
+
+- [Mapbox GL JS](https://docs.mapbox.com/mapbox-gl-js/api/)
+- [Bootstrap Icons](https://icons.getbootstrap.com/)
+- [scroolama](https://github.com/russellsamora/scrollama)
+
+## Acknowledgements
+
+We would like to thank the New York Times for providing the COVID-19 data that we used in our project. We would also like to thank Professor Bo Zhao, and TA Liz Peng for their help and support throughout the quarter.
--- js/main.js
+++ js/main.js
@@ -1,72 +1,265 @@
-// Main JavaScript file
-
-// 1. Declare the maps, script panels, and different thematic layers.
-let map, scriptPanel = scrollama(), countiesLayer;
-
-// 2. Initialize the layout.
-// ... 
-
-// 3. Define Generic window resize listener event
+let map, scriptPanel = scrollama(), stateLayer, countyLayer, sceneIndex = 0;
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+history.scrollRestoration = "manual";
+window.scrollTo(0, 0);
+adjustStoryboardlSize();
+window.addEventListener("resize", adjustStoryboardlSize);
 function adjustStoryboardlSize() {
-  // ...
+  const scenes = document.getElementsByClassName("scene");
+  const storyboard = document.getElementById("storyboard");
+  let sceneH = Math.floor(window.innerHeight * 0.75);
+  for (const scene of scenes) {
+    scene.style.height = sceneH + "px";
+  }
+  let storyboardHeight = window.innerHeight;
+  let storyboardMarginTop = (window.innerHeight - storyboardHeight) / 2;
+  storyboard.style.height = storyboardHeight + "px";
+  storyboard.style.top = storyboardMarginTop + "px"
+  scriptPanel.resize();
 }
-
-// 4. Initialize the mapbox
-// ...
-
-// 5. Define the asynchronous function to load geojson data and then performs the dependent actions.
+map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  zoom: 4,
+  minZoom: 1,
+  maxZoom: 10,
+  center: [-98.93, 40.33],
+  scrollZoom: false,
+  boxZoom: false,
+  doubleClickZoom: false
+});
 async function geojsonFetch() {
-  // ...
-
-  // 7. Trigger operations inside of the ()=> {} funciton while loading the map.
+  let response, state, county;
+  response = await fetch("assets/state_data.geojson");
+  state = await response.json();
+  response = await fetch("assets/us_covid_2020_rates.json");
+  county = await response.json();
   map.on('load', () => {
-    // 8. Add map source and declare layers.
-    // ...
-
-    // 9. Initialize the script panel
+    map.addSource('county-src', {
+      type: 'geojson',
+      data: county
+    });
+    map.addSource('state-src', {
+      type: 'geojson',
+      data: state
+    });
+    stateLayer = {
+      'id': 'state-polygons',
+      'type': 'fill',
+      'source': 'state-src',
+      'minzoom': 5,
+      'paint': {
+        'fill-color': '#0080ff',
+        'fill-opacity': 0.5
+      }
+    };
+    countyLayer = {
+      'id': 'county-points',
+      'type': 'fill',
+      'source': 'county-src',
+      'minzoom': 3,
+      'paint': {
+        'fill-color': [
+          'interpolate',
+          ['linear'],
+          ['get', 'cases'],
+          2500, '#fc9272',
+          5000, '#fb6a4a',
+          10000, '#ef3b2c',
+          20000, '#cb181d',
+          50000, '#a50f15',
+          60000, '#67000d'
+        ],
+        'fill-opacity': 0.5
+      }
+    };
     scriptPanel
       .setup({
-        step: ".scene", // all the scenes.
-        offset: 0.33, // the location of the enter and exit trigger
-        debug: false // toggler on or off the debug mode.
+        step: ".scene",
+        offset: 0.33,
+        debug: false
       })
       .onStepEnter(handleSceneEnter)
-      .onStepExit(handleSceneExit);
-
-    // 10. This function performs when a scene enters the storyboard
+      .onStepExit(handleSceneExit)
     function handleSceneEnter(response) {
-      var index = response.index; // Capture the id of the current scene.
-
+      let index = response.index;
+      sceneIndex = response.index;
       if (index === 0) {
-        // When enter the first scene
         map.flyTo({
-          center: [-121.93, 47.33],
-          zoom: 8,
+          center: [-123.93, 47.33],
+          zoom: 6,
           pitch: 0,
-          speed: 0.5
+          speed: 1.2
         });
-        // ...
-      } else if (index === 1) {
-        // When enter the second scene
-        // ...
+        if (typeof (map.getSource('state-src')) == 'undefined') {
+          map.addSource('state-src', {
+            type: 'geojson',
+            data: state
+          });
+        } else {
+          map.getSource('state-src').setData(state);
+        }
+        if (!map.getLayer("state-polygons")) {
+          map.addLayer(stateLayer);
+        }
+        document.getElementById("cover").style.visibility = "hidden";
+        map.setFilter('state-polygons', ['==', 'name', 'Washington']);
+      } else if (index === 1 || index === 5) {
+        if (index === 1) {
+          map.setFilter('state-polygons', ['==', 'name', 'New York']);
+        }
+        map.flyTo({
+          center: [-78.93, 43.33],
+          zoom: 6,
+          pitch: 0,
+          speed: 1.5
+        });
+        if (index === 1) {
+          map.setFilter('state-polygons', ['==', 'name', 'New York']);
+        }
+      } else if (index === 2 || index === 6) {
+        if (index === 2) {
+          map.setFilter('state-polygons', ['==', 'name', 'Georgia']);
+        }
+        map.flyTo({
+          center: [-84.93, 32.33],
+          zoom: 6,
+          pitch: 0,
+          speed: 0.8
+        });
+      } else if (index === 3 || index === 7) {
+        if (index === 3) {
+          map.setFilter('state-polygons', ['==', 'name', 'Florida']);
+        }
+        map.flyTo({
+          center: [-83.93, 27.33],
+          zoom: 6,
+          pitch: 0,
+          speed: 0.8
+        });
+      } else if (index === 4) {
+        map.flyTo({
+          center: [-123.93, 47.33],
+          zoom: 6,
+          pitch: 0,
+          speed: 0.8
+        });
+        if (typeof (map.getSource('county-src')) == 'undefined') {
+          map.addSource('county-src', {
+            type: 'geojson',
+            data: county
+          });
+        } else {
+          map.getSource('county-src').setData(county);
+        }
+        if (!map.getLayer("county-points")) {
+          map.addLayer(countyLayer);
+        }
+      } else if (index === 8) {
+        map.flyTo({
+          center: [-98.93, 40.33],
+          zoom: 4,
+          pitch: 0,
+          speed: 0.8
+        });
       }
-      // ... Handle other scenes
     }
-
-    // 11. This function performs when a scene exits the storyboard
     function handleSceneExit(response) {
-      var index = response.index;
+      let index = response.index;
       if (index === 0) {
-        // When exit the first scene
-        // ...
+        if (response.direction == 'down') {
+          document.getElementById("cover").style.visibility = "hidden";
+          map.setFilter('state-polygons', ['==', 'name', 'Washington']);
+        } else {
+          document.getElementById("cover").style.visibility = "visible";
+        }
       } else if (index === 1) {
-        // When exit the second scene
-        // ...
+        if (response.direction == 'up') {
+          map.setFilter('state-polygons', ['==', 'name', 'Washington']);
+        } else {
+          map.setFilter('state-polygons', ['==', 'name', 'New York']);
+        }
+      } else if (index === 2) {
+        if (response.direction == 'up') {
+          map.setFilter('state-polygons', ['==', 'name', 'New York']);
+        } else {
+          map.setFilter('state-polygons', ['==', 'name', 'Georgia']);
+        }
+      } else if (index === 3) {
+        if (response.direction == 'down') {
+          if (map.getLayer("state-polygons")) {
+            map.removeLayer('state-polygons');
+          }
+        } else {
+          map.setFilter('state-polygons', ['==', 'name', 'Georgia']);
+        }
+      } else if (index === 4) {
+        if (response.direction == 'up') {
+          if (map.getLayer("county-points")) {
+            map.removeLayer('county-points');
+          }
+          if (typeof (map.getSource('state-src')) == 'undefined') {
+            map.addSource('state-src', {
+              type: 'geojson',
+              data: state
+            });
+          } else {
+            map.getSource('state-src').setData(state);
+          }
+          if (!map.getLayer("state-polygons")) {
+            map.addLayer(stateLayer);
+          }
+          map.setFilter('state-polygons', ['==', 'name', 'Florida']);
+        }
+      } else if (index === 8) {
+        if (response.direction == 'up') {
+          if (typeof (map.getSource('county-src')) == 'undefined') {
+            map.addSource('county-src', {
+              type: 'geojson',
+              data: county
+            });
+          } else {
+            map.getSource('county-src').setData(county);
+          }
+          if (!map.getLayer("county-points")) {
+            map.addLayer(countyLayer);
+          }
+        }
       }
-      // ... Handle other scenes
     }
   });
-}
-
-// 5. Call the data loading function.
+  map.on('mousemove', ({ point }) => {
+    let countyFeatures = map.queryRenderedFeatures(point);
+    countyFeatures = countyFeatures.filter(feature => feature.layer.id === 'county-points');
+    const scene4 = document.querySelector('[data-scene="4"]');
+    const scene5 = document.querySelector('[data-scene="5"]');
+    const scene6 = document.querySelector('[data-scene="6"]');
+    const scene7 = document.querySelector('[data-scene="7"]');
+    if (countyFeatures.length > 0) {
+      const county = countyFeatures[0].properties.county;
+      const cases = countyFeatures[0].properties.cases;
+      if (sceneIndex === 4) {
+        const hoverInfo = scene4.querySelector('h3');
+        hoverInfo.textContent = `${county} County: Total Cases ${cases}`;
+      }
+      if (sceneIndex === 5) {
+        const hoverInfo = scene5.querySelector('h3');
+        hoverInfo.textContent = `${county} County: Total Cases ${cases}`;
+      }
+      if (sceneIndex === 6) {
+        const hoverInfo = scene6.querySelector('h3');
+        hoverInfo.textContent = `${county} County: Total Cases ${cases}`;
+      }
+      if (sceneIndex === 7) {
+        const hoverInfo = scene7.querySelector('h3');
+        hoverInfo.textContent = `${county} County: Total Cases ${cases}`;
+      }
+    } else {
+      const hoverInfo = document.querySelectorAll('h3');
+      hoverInfo.forEach(info => {
+        info.textContent = "Hover over a County for more Info!";
+      });
+    }
+  });
+};
 geojsonFetch();
